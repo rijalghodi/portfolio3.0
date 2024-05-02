8675172004f37158f7497e3d7e14bb08035369a9
Rijal Ghodi <rijalghodi10@gmail.com>
2024-05-03 00:28:42 +0700

    add paper studio

--- src/data/experience.ts
+++ src/data/experience.ts
@@ -2,14 +2,24 @@ import { Experience } from '@/types';
 
 export const experiences: Experience[] = [
   {
+    position: 'Tech Leader',
+    company: 'Studio Inovasi Indonesia',
+    experienceCategory: 'Full Time',
+    startDate: 'Mar 2022',
+    endDate: 'Now',
+    logo: '/companies/paperstudio.png',
+    shortDesc:
+      'Creates an innovative and intuitive tools to enhance Indonesian academic papers',
+  },
+  {
     position: 'Front End Developer',
     company: 'PT. Optima Media Teknologi (Optimap)',
     companyLink: 'https://dev.optimap.id',
     experienceCategory: 'Full Time',
     startDate: 'Dec 2022',
-    endDate: 'Now',
+    endDate: 'Mar 2023',
     logo: '/companies/optimap.svg',
     shortDesc:
-      'Optimap is company that help other companies to optimize their map digitally',
+      'IT consulting and mapping services, specializing in Geographical Information System expertise.',
   },
 ];
